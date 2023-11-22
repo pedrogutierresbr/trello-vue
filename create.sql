7393b8d76e6871e63805b0ced4231337f5e8eb8a
Pedro Gutierres <78659993+pedrogutierresbr@users.noreply.github.com>
2023-11-21 21:10:03 -0300

    inserido dados no banco e criado rotas para recuperar dados
    adicionado testes de integracao

--- create.sql
+++ create.sql
@@ -21,5 +21,10 @@ create table pedro.card (
     estimative integer
 );
 
-
-
+insert into pedro.board (id_board, name) values (1, 'Projeto 1');
+insert into pedro.column (id_column, id_board, name, has_estimative) values ( 1, 1, 'Coluna A', true);
+insert into pedro.column (id_column, id_board, name, has_estimative) values ( 2, 1, 'Coluna B', true);
+insert into pedro.column (id_column, id_board, name, has_estimative) values ( 3, 1, 'Coluna C', true);
+insert into pedro.card (id_card, id_column, title, estimative) values (1, 1, 'Atividade 1', 3);
+insert into pedro.card (id_card, id_column, title, estimative) values (2, 1, 'Atividade 2', 2);
+insert into pedro.card (id_card, id_column, title, estimative) values (3, 1, 'Atividade 3', 1);
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,28 @@
 import express from "express";
+import pgp from "pg-promise"
 
 const app = express();
-app.get("/boards", function (req, res) {
-	const boards = [{ name: "Projeto 1" }];
+const connection = pgp()("postgres://postgres:197634@localhost:5432/trello")
+
+app.get("/boards", async function (req, res) {
+	const boards = await connection.query("select * from pedro.board", [])
+	console.log(boards);
+	
 	res.json(boards);
+});
+
+app.get("/boards/:idBoard/columns", async function (req, res) {
+	const columns = await connection.query("select * from pedro.column where id_board = $1", [req.params.idBoard])
+	console.log(columns);
+	
+	res.json(columns);
+});
+
+app.get("/boards/:idBoard/columns/:idColumn/cards", async function (req, res) {
+	const cards = await connection.query("select * from pedro.card where id_column = $1", [req.params.idColumn])
+	console.log(cards);
+	
+	res.json(cards);
 });
 
 app.listen(3000);
