43b57726a00aaf5d0f63c7216ed4ece37317f0d1
Pedro Gutierres <78659993+pedrogutierresbr@users.noreply.github.com>
2023-12-05 21:03:14 -0300

    refatorado
    criado um contrato Http;
    criado um adapter para abstrair o necessário do express;
    retirado rotas do main.ts e recolocado em um local proprio;

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,8 @@
-import express from "express";
-import BoardService from "./service/BoardService";
-import ColumnService from "./service/ClumnsService";
-import CardService from "./service/CardService";
+import BoardController from "./infra/controller/BoardController";
 import PgPromiseConnection from "./infra/database/PgPromiseConnection";
-import BoardRepositoryDatabase from "./infra/repository/BoardRepositoryDatabase";
-import ColumnRepositoryDatabase from "./infra/repository/ColumnRepositoryDatabase";
-import CardRepositoryDatabase from "./infra/repository/CardRepositoryDatabase";
-
-const app = express();
+import ExpressAdapter from "./infra/http/ExpressAdapter";
 
 const connection = new PgPromiseConnection()
-const boardRepository = new BoardRepositoryDatabase(connection)
-const columnRepository = new ColumnRepositoryDatabase(connection)
-const cardRepository = new CardRepositoryDatabase(connection)
-
-app.get("/boards", async function (req, res) {
-	const boardService = new BoardService(boardRepository)
-	const boards = await boardService.getBoards()
-	res.json(boards);
-});
-
-app.get("/boards/:idBoard/columns", async function (req, res) {
-	const columnService = new ColumnService(columnRepository)
-	const columns = await columnService.getColumns(parseInt(req.params.idBoard))
-	res.json(columns);
-});
-
-app.get("/boards/:idBoard/columns/:idColumn/cards", async function (req, res) {
-	const cardService = new CardService(cardRepository)
-	const cards = await cardService.getCards(parseInt(req.params.idColumn))	
-	res.json(cards);
-});
-
-app.listen(3000);
+const http = new ExpressAdapter()
+new BoardController(http, connection)
+http.listen(3000)
